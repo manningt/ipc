fa17017d20d6d1fff70f4416150c2d24fec2434f
Tom Manning <tom@manningetal.com>
2021-01-31 09:57:32 -0500

    remove protobufs; update #defines to be translated to python

--- ipc_control.c
+++ ipc_control.c
@@ -16,10 +16,6 @@
 #include "logging.h"
 #include "faults.h"
 
-#include <pb_encode.h>
-#include <pb_decode.h>
-#include "message.pb.h"
-
 #ifdef IPC_CONTROL_TESTING
 #include "globals_for_test.h"
 #endif
@@ -33,24 +29,23 @@ char * other_name = "Ctrl";
 
 char plog_string[256];
 
-// uint8_t pbbuffer[MAX_MESSAGE_SIZE] = {0};
-// int in_pbbuffer_len, out_pbbuffer_len;
-
 uint8_t out_message[MAX_MESSAGE_SIZE] ;
 uint8_t out_message_len;
 int out_response_int;
-uint8_t in_message[MAX_MESSAGE_SIZE];
+int out_buffer_len;
 
 uint8_t buffer[MAX_MESSAGE_SIZE] = {0};
-int out_buffer_len;
-int byte_count_rcvd = 0;
-int byte_count_sent = 0;
+int byte_count_rcvd;
+int byte_count_sent;
 
 void ipc_control_init()
 {
 	if (ipc_control_initialized == 0)
 	{
-		mode_settings.mode = mode_e_GAME;
+		mode_settings.mode = GAME_MODE_E;
+		ipc_control_statistics[0].num_bad_msgs = 0;
+		ipc_control_statistics[0].num_read_msgs = 0;
+		ipc_control_statistics[0].num_write_msgs = 0;
 	}
 	if (ipc_transport_init(&ipc_transport_desc[0], my_name, other_name) < 0)
 	{
@@ -58,10 +53,6 @@ void ipc_control_init()
 	}
 	else
 		LOG_DEBUG( "control ipc init OK.");
-
-	ipc_control_statistics[0].num_bad_msgs = 0;
-	ipc_control_statistics[0].num_read_msgs = 0;
-	ipc_control_statistics[0].num_write_msgs = 0;
 	ipc_control_initialized = 1;
 }
 
@@ -76,41 +67,41 @@ int encode_mode() {
 }
 
 int decode_mode() {
-	printf("decoding: %s\n", buffer);
+	// LOG_DEBUG("decoding: %s\n", buffer);
 	char * item_ptr;
 	char * end_item_ptr;
 	char * buff_ptr = (char * ) buffer;
-	buff_ptr += MESSAGE_BODY_OFFSET;  //skip over header
+	buff_ptr += HEADER_LENGTH+1;  //skip over header
 	item_ptr = strtok_r ( buff_ptr,",", &end_item_ptr);
 	while (item_ptr != NULL)
 	{
 		char * kv_end;
 		char *key_ptr = strtok_r(item_ptr, ":", &kv_end);
 		char *value_ptr = strtok_r(NULL, ":", &kv_end);
-		// printf("key: %s   Value: %s   case: %c\n", key_ptr, value_ptr, key_ptr[0]);
+		// LOG_DEBUG("key: %s   Value: %s   case: %c\n", key_ptr, value_ptr, key_ptr[0]);
 		switch(key_ptr[0]) {
 			case 'm':
 				mode_settings.mode = atoi(value_ptr);
-				printf("mode = %d\n", mode_settings.mode);
+				//LOG_DEBUG("mode = %d\n", mode_settings.mode);
 				break;
 			case 'i':
 				mode_settings.drill_workout_id = atoi(value_ptr);
-				printf("drill id = %d\n", mode_settings.drill_workout_id );
+				//LOG_DEBUG("drill id = %d\n", mode_settings.drill_workout_id );
 				break;
 			case 's':
 				mode_settings.drill_step = atoi(value_ptr);
-				printf("step id = %d\n",mode_settings.drill_step);
+				//LOG_DEBUG("step id = %d\n",mode_settings.drill_step);
 				break;
 			case 'd':
 				doubles_mode = atoi(value_ptr);
-				printf("doubles = %d\n", doubles_mode);
+				//LOG_DEBUG("doubles = %d\n", doubles_mode);
 				break;
 			case 't':
 				FAKE_tiebreak_mode = atoi(value_ptr);
-				printf("tiebreaker = %d\n", FAKE_tiebreak_mode);
+				//LOG_DEBUG("tiebreaker = %d\n", FAKE_tiebreak_mode);
 				break;
 			default:
-			printf("Unrecognized key: %s in: %s\n", item_ptr, buffer);
+			LOG_DEBUG("Unrecognized key: %s in: %s\n", item_ptr, buffer);
 			break;
 		}
 		item_ptr = strtok_r (NULL, "," , &end_item_ptr);
@@ -123,37 +114,37 @@ int encode_parms() {
 }
 
 int decode_parms() {
-	// printf("decoding: %s\n", buffer);
+	// LOG_DEBUG("decoding: %s\n", buffer);
 	char * item_ptr;
 	char * end_item_ptr;
 	char * buff_ptr = (char * ) buffer;
-	buff_ptr += MESSAGE_BODY_OFFSET;  //skip over header
+	buff_ptr += HEADER_LENGTH+1;  //skip over header
 	item_ptr = strtok_r ( buff_ptr,",", &end_item_ptr);
 	while (item_ptr != NULL)
 	{
 		char * kv_end;
 		char *key_ptr = strtok_r(item_ptr, ":", &kv_end);
 		char *value_ptr = strtok_r(NULL, ":", &kv_end);
-		// printf("key: %s   Value: %s   case: %c\n", key_ptr, value_ptr, key_ptr[0]);
+		// LOG_DEBUG("key: %s   Value: %s   case: %c\n", key_ptr, value_ptr, key_ptr[0]);
 		switch(key_ptr[0]) {
 			case 'l':
 				boomer_level = atoi(value_ptr);
-				printf("level = %d\n",boomer_level);
+				// LOG_DEBUG("level = %d\n",boomer_level);
 				break;
 			case 's':
 				speed_mod = atoi(value_ptr);
-				printf("speed = %d\n", speed_mod );
+				// LOG_DEBUG("speed = %d\n", speed_mod );
 				break;
 			case 'h':
 				height_mod = atoi(value_ptr);
-				printf("height = %d\n",height_mod);
+				// LOG_DEBUG("height = %d\n",height_mod);
 				break;
 			case 'd':
 				delay_mod = atoi(value_ptr);
-				printf("delay = %d\n", delay_mod);
+				// LOG_DEBUG("delay = %d\n", delay_mod);
 				break;
 			default:
-			printf("Unrecognized key: %s in: %s\n", item_ptr, buffer);
+			LOG_DEBUG("Unrecognized key: %s in: %s\n", item_ptr, buffer);
 			break;
 		}
 		item_ptr = strtok_r (NULL, "," , &end_item_ptr);
@@ -162,12 +153,12 @@ int decode_parms() {
 }
 
 int decode_cam_calibration(uint8_t camera_id) {
-	printf("decoding: %s\n", buffer);
+	// LOG_DEBUG("decoding: %s\n", buffer);
 	// camera_id doesn't have to be checked for bounds, since it's generated in the RSRC decode
 	char * item_ptr;
 	char * end_item_ptr;
 	char * buff_ptr = (char * ) buffer;
-	buff_ptr += MESSAGE_BODY_OFFSET;  //skip over header
+	buff_ptr += HEADER_LENGTH+1;  //skip over header
 	item_ptr = strtok_r ( buff_ptr,",", &end_item_ptr);
 	uint8_t pt_index;
 	bool is_y_point;
@@ -176,7 +167,7 @@ int decode_cam_calibration(uint8_t camera_id) {
 		char * kv_end;
 		char *key_ptr = strtok_r(item_ptr, ":", &kv_end);
 		char *value_ptr = strtok_r(NULL, ":", &kv_end);
-		printf("key: %s   Value: %s\n", key_ptr, value_ptr);
+		// LOG_DEBUG("key: %s   Value: %s\n", key_ptr, value_ptr);
 		if ((key_ptr[0] < POINT_START_CHAR) || (key_ptr[0] > (POINT_START_CHAR+(NUM_CAM_CALIB_POINTS*2)-1)))
 		{
 			LOG_DEBUG("Unrecognized key: %s in: %s\n", item_ptr, buffer);
@@ -229,272 +220,63 @@ void ipc_control_update() {
 			LOG_DEBUG("read pipe closed.");
 		}
 		switch(buffer[0]) {
-			case 'G': {
+			case 'G':
 				//handle GET
-				if (!memcmp(buffer+RSRC_OFFSET, RESOURCE_STRING[STAT], RSRC_STRING_LENGTH)) {
+				if (!memcmp(buffer+RSRC_OFFSET, STAT_RSRC, RSRC_STRING_LENGTH)) {
 					out_message_len = encode_status();
 				}
-				else if (!memcmp(buffer+RSRC_OFFSET, RESOURCE_STRING[MODE], RSRC_STRING_LENGTH)) {
+				else if (!memcmp(buffer+RSRC_OFFSET, MODE_RSRC, RSRC_STRING_LENGTH)) {
 					out_message_len = encode_mode();
 				}
-				else if (!memcmp(buffer+RSRC_OFFSET, RESOURCE_STRING[PARM], RSRC_STRING_LENGTH)) {
+				else if (!memcmp(buffer+RSRC_OFFSET, LDSH_RSRC, RSRC_STRING_LENGTH)) {
 					out_message_len = encode_parms();
 				}
-				else if (!memcmp(buffer+RSRC_OFFSET, RESOURCE_STRING[LCAM], RSRC_STRING_LENGTH)) {
+				else if (!memcmp(buffer+RSRC_OFFSET, LCAM_RSRC, RSRC_STRING_LENGTH)) {
 					out_message_len = encode_cam_calibration(LEFT_CAMERA_ID);
 				}
-			}
 				break;
 			case 'P':
 				//handle PUT
-				if (!memcmp(buffer+RSRC_OFFSET, RESOURCE_STRING[MODE], RSRC_STRING_LENGTH)) {
+				if (!memcmp(buffer+RSRC_OFFSET, MODE_RSRC, RSRC_STRING_LENGTH)) {
 					// can only change the mode config params if boomer is not active
 					if (BASE_ACTIVE == 0)
 						out_response_int = decode_mode();
 					else
 						out_response_int = LOCKED;
 				}
-				else if (!memcmp(buffer+RSRC_OFFSET, RESOURCE_STRING[PARM], RSRC_STRING_LENGTH)) {
+				else if (!memcmp(buffer+RSRC_OFFSET, LDSH_RSRC, RSRC_STRING_LENGTH)) {
 					out_response_int = decode_parms();
 				}
-				else if (!memcmp(buffer+RSRC_OFFSET, RESOURCE_STRING[LCAM], RSRC_STRING_LENGTH)) {
+				else if (!memcmp(buffer+RSRC_OFFSET, LCAM_RSRC, RSRC_STRING_LENGTH)) {
 					out_response_int = decode_cam_calibration(LEFT_CAMERA_ID);
 				}
-				else if (!memcmp(buffer+RSRC_OFFSET, RESOURCE_STRING[STRT], RSRC_STRING_LENGTH))
+				else if (!memcmp(buffer+RSRC_OFFSET, STRT_RSRC, RSRC_STRING_LENGTH))
 				{
-					if (mode_settings.mode == mode_e_GAME) start_game();
-					else if (mode_settings.mode == mode_e_DRILL)
+					if (mode_settings.mode == GAME_MODE_E) start_game();
+					else if (mode_settings.mode == DRILL_MODE_E)
 					{
 						load_drill( (int16_t) mode_settings.drill_workout_id);
 						start_drill();
 					}
 					else LOG_ERROR("Invalid mode on start: %d", mode_settings.mode);
 				}
-				else if (!memcmp(buffer+RSRC_OFFSET, RESOURCE_STRING[STOP], RSRC_STRING_LENGTH))
+				else if (!memcmp(buffer+RSRC_OFFSET, STOP_RSRC, RSRC_STRING_LENGTH))
 				{
 					if (game_state != IDLE_GS) end_game();
 					else if (drill_state != IDLE_DS) end_drill();
 				}
-
-
 				break;
 			/*
 			handle responses when requests are made.
 			case 'R':
-				printf("Got response %s\n", buffer);
 				break;
 			*/
 			default:
 				LOG_DEBUG("Unrecognized request: %s", buffer);
 		}
 
-/*
-		else if (memcmp(buffer, PROTOCOL_ID, sizeof(PROTOCOL_ID)-1) != 0)
-      {
-        LOG_ERROR( "BIPC not in header.");
-        out_response_int = BAD_REQUEST;
-        ipc_control_statistics[0].num_bad_msgs++;
-      }
-      else if (!memcmp(buffer+sizeof(PROTOCOL_ID), METHOD_STRING[GET], METHOD_STRING_LENGTH))
-      {
-			// Process GETs
-			if (!memcmp(buffer+RSRC_OFFSET, RESOURCE_STRING[STAT], RSRC_STRING_LENGTH))
-			{
-				LOG_DEBUG("GET STATU");
-				b_status_msg msg_status = b_status_msg_init_zero;
-				msg_status.active = BASE_ACTIVE;
-				msg_status.soft_fault = soft_fault;
-				msg_status.hard_fault = hard_fault;
-				pb_ostream_t stream = pb_ostream_from_buffer(pbbuffer, sizeof(pbbuffer));
-				if (!pb_encode(&stream, b_status_msg_fields, &msg_status))
-				{
-					sprintf(plog_string, "Encoding failed on STATUS message: %s\n", PB_GET_ERROR(&stream));
-					LOG_ERROR( plog_string);
-					out_response_int = UNPROCESSABLE_ENTITY;
-					// TODO:  handle encode errors
-				}
-				out_pbbuffer_len = stream.bytes_written;
-			}
-			else if (!memcmp(buffer+RSRC_OFFSET, RESOURCE_STRING[MODE], RSRC_STRING_LENGTH))
-			{
-				LOG_DEBUG("GET MODE");
-				b_mode_msg msg_mode = b_mode_msg_init_zero;
-				msg_mode.mode = mode_settings.mode;
-				msg_mode.drill_workout_id = mode_settings.drill_workout_id;
-				msg_mode.drill_step = mode_settings.drill_step;
-				msg_mode.doubles = doubles_mode;
-				// msg_mode.tie_breaker = tiebreak_mode;
-				msg_mode.tie_breaker = mode_settings.temporary_tie_breaker_mode;
-				// msg_mode.iterations = mode_settings.iterations;
-				pb_ostream_t stream = pb_ostream_from_buffer(pbbuffer, sizeof(pbbuffer));
-				if (!pb_encode(&stream, b_mode_msg_fields, &msg_mode))
-				{
-					sprintf(plog_string, "Encoding failed on MODE message: %s\n", PB_GET_ERROR(&stream));
-					LOG_ERROR( plog_string);
-					// TODO: handle encode errors
-					out_response_int = UNPROCESSABLE_ENTITY;
-				}
-				out_pbbuffer_len = stream.bytes_written;
-			}
-			else if (!memcmp(buffer+RSRC_OFFSET, RESOURCE_STRING[PARM], RSRC_STRING_LENGTH))
-			{
-				LOG_DEBUG("GET PARMS");
-				b_params_msg params = b_params_msg_init_zero;
-				if (mode_settings.mode == mode_e_GAME)
-				{
-					params.level = boomer_level;
-				}
-				else
-				{
-					params.level = level_mod;
-				}
-				params.speed = speed_mod;
-				params.height = height_mod;
-				params.delay = frequency_mod;
-				pb_ostream_t stream = pb_ostream_from_buffer(pbbuffer, sizeof(pbbuffer));
-				if (!pb_encode(&stream, b_params_msg_fields, &params))
-				{
-					sprintf(plog_string, "Encoding failed on PARMS message: %s\n", PB_GET_ERROR(&stream));
-					LOG_ERROR( plog_string);
-					// TODO: handle encode errors
-					out_response_int = 400;
-				}
-				out_pbbuffer_len = stream.bytes_written;
-			}
-			else
-			{
-				sprintf(plog_string, "Unrecognized resource for GET: %s", buffer);
-				LOG_ERROR( plog_string);
-				ipc_control_statistics[0].num_bad_msgs++;
-			}
-		} else if (!memcmp(buffer+sizeof(PROTOCOL_ID), METHOD_STRING[PUT], METHOD_STRING_LENGTH))
-		{
-			// Process PUT messages
-			if (byte_count_rcvd > BIPC_HEADER_LENGTH)
-			{
-				//copy out message portion for decoding
-				in_pbbuffer_len = byte_count_rcvd - 14; // why does the define not work?? BIPC_HEADER_LENGTH;
-				memcpy(pbbuffer, buffer+BIPC_HEADER_LENGTH, in_pbbuffer_len);
-			}
-			else in_pbbuffer_len = 0;
-
-			if (!memcmp(buffer+RSRC_OFFSET, RESOURCE_STRING[STRT], RSRC_STRING_LENGTH))
-			{
-				LOG_DEBUG("PUT START");
-				if (mode_settings.mode == mode_e_GAME) start_game();
-				else if (mode_settings.mode == mode_e_DRILL)
-				{
-					load_drill( (int16_t) mode_settings.drill_workout_id);
-					start_drill();
-				}
-				else LOG_ERROR("Invalid mode on start: %d", mode_settings.mode);
-			}
-			else if (!memcmp(buffer+RSRC_OFFSET, RESOURCE_STRING[STOP], RSRC_STRING_LENGTH))
-			{
-				LOG_DEBUG("PUT STOP");
-				if (game_state != IDLE_GS) end_game();
-				else if (drill_state != IDLE_DS) end_drill();
-
-			}
-			else if (!memcmp(buffer+RSRC_OFFSET, RESOURCE_STRING[MODE], RSRC_STRING_LENGTH))
-			{
-				LOG_DEBUG("PUT MODE");
-				if (BASE_ACTIVE == 0)
-				{
-					// can only change the mode config params if boomer is not active
-					if (in_pbbuffer_len < 1)
-					{
-						sprintf(plog_string, "No message component for: %s", buffer);
-						LOG_ERROR( plog_string);
-						ipc_control_statistics[0].num_bad_msgs++;
-						out_response_int = BAD_REQUEST;
-					}
-					else
-					{
-						b_mode_msg in_message = b_mode_msg_init_zero;
-						pb_istream_t stream = pb_istream_from_buffer(pbbuffer, in_pbbuffer_len);
-						if (!pb_decode(&stream, b_mode_msg_fields, &in_message))
-						{
-							sprintf(plog_string, "Decode of PUT MODE parameters failed: %s", PB_GET_ERROR(&stream));
-							LOG_ERROR( plog_string);
-							// TODO: handle decode errors
-							out_response_int = BAD_REQUEST;
-							ipc_control_statistics[0].num_bad_msgs++;
-						} else 
-						{
-							// if a parameter is not populated in the message, it's value will be 0 in the decoded message
-							if (in_message.mode != mode_e_MODE_UNKNOWN) mode_settings.mode = in_message.mode;
-							mode_settings.drill_workout_id = in_message.drill_workout_id;
-							mode_settings.drill_step = in_message.drill_step;
-							doubles_mode =	in_message.doubles;
-							// tiebreak_mode = in_message.tie_breaker;
-							mode_settings.temporary_tie_breaker_mode = in_message.tie_breaker;
-							// mode_settings.iterations = in_message.iterations;
-						}
-					}
-				}
-				else
-				{
-					LOG_WARNING("PUT MODE rejected because boomer_base is Active\n");
-					out_response_int = LOCKED;
-				}
-			}
-			else if (!memcmp(buffer+RSRC_OFFSET, RESOURCE_STRING[PARM], RSRC_STRING_LENGTH))
-			{
-				LOG_DEBUG("PUT PARMS");
-				if (in_pbbuffer_len < 1)
-				{
-					sprintf(plog_string, "No message component for: %s", buffer);
-					LOG_ERROR( plog_string);
-					ipc_control_statistics[0].num_bad_msgs++;
-					out_response_int = BAD_REQUEST;
-				}
-				else
-				{
-					b_params_msg in_message = b_params_msg_init_zero;
-					pb_istream_t stream = pb_istream_from_buffer(pbbuffer, in_pbbuffer_len);
-					if (!pb_decode(&stream, b_params_msg_fields, &in_message))
-					{
-						sprintf(plog_string, "Decode of PUT PARMS parameters failed: %s", PB_GET_ERROR(&stream));
-						LOG_ERROR( plog_string);
-						// TODO: handle decode errors
-						out_response_int = BAD_REQUEST;
-						ipc_control_statistics[0].num_bad_msgs++;
-					}
-					else 
-					{
-						if (mode_settings.mode == mode_e_GAME)
-						{
-							boomer_level = (uint8_t) in_message.level;
-						}
-						else
-						{
-							level_mod = (int8_t) in_message.level;
-						}
-						speed_mod = (uint8_t) in_message.speed;
-						height_mod = (int8_t) in_message.height;
-						frequency_mod = (uint16_t) in_message.delay;
-					}
-				}
-			}
-			else
-			{
-				sprintf(plog_string, "Illegal resource for PUT: %s", buffer);
-				LOG_WARNING( plog_string);
-				out_response_int = NOT_FOUND;
-				ipc_control_statistics[0].num_bad_msgs++;
-			}
-		}
-		else
-		{
-			sprintf(plog_string, "Unrecognized method: %s", buffer);
-			LOG_WARNING( plog_string);
-			out_response_int = METHOD_NOT_ALLOWED;
-			ipc_control_statistics[0].num_bad_msgs++;
-		}
-*/
 		// send response
-			out_buffer_len = sprintf((char *)buffer, "%s  %3d", METHOD_STRING[RSP], out_response_int);
+		out_buffer_len = sprintf((char *)buffer, "%s  %3d", RSP_METHOD, out_response_int);
 		if (out_message_len > 0)
 		{
 			strcat((char *) buffer, (char *)out_message);
--- ipc_control.h
+++ ipc_control.h
@@ -7,56 +7,34 @@ extern "C" {
 #include "ipc_fifo_transport.h"
 #include <pthread.h>
 
-//--start of shared defines --//
+//-=-=- start of exported defines
+#define MAX_MESSAGE_SIZE 128
 
 #define RSRC_STRING_LENGTH 4
 #define RSRC_OFFSET 4
-#define MESSAGE_BODY_OFFSET 9
+#define HEADER_LENGTH 8
+
+#define GET_METHOD "GET"
+#define PUT_METHOD "PUT"
+#define RSP_METHOD "RSP"
+
+#define STAT_RSRC "STAT"
+#define STOP_RSRC "STOP"
+#define STRT_RSRC "STRT"
+#define MODE_RSRC "MODE"
+#define LDSH_RSRC "LDSH"  //level, delay, speed, height - can be changed on the fly
+#define LCAM_RSRC "LCAM"  //left cam
+#define RCAM_RSRC "RCAM"  //right cam
+
+// #define MODE_E_UNKNOWN 0
+#define GAME_MODE_E 1
+#define DRILL_MODE_E 2
+#define WORKOUT_MODE_E 3
 
 #define NUM_CAM_CALIB_POINTS 7
 #define POINT_START_CHAR 'a'
 #define LEFT_CAMERA_ID 0
 #define RIGHT_CAMERA_ID 1
-
-#define MAX_MESSAGE_SIZE 128
-
-#define PROTOCOL_ID ""
-
-#define GENERATE_ENUM(ENUM) ENUM,
-#define GENERATE_STRING(STRING) #STRING,
-
-
-#define FOREACH_RESOURCE(RSRC) \
-			RSRC(INVALID_RESOURCE)  \
-			RSRC(STAT)  \
-			RSRC(STRT)  \
-			RSRC(STOP) \
-			RSRC(MODE) \
-			RSRC(PARM) \
-			RSRC(LCAM)
-
-typedef enum RESOURCE_ENUM {
-	FOREACH_RESOURCE(GENERATE_ENUM)
-} resource_t;
-
-static const char *RESOURCE_STRING[] = {
-		FOREACH_RESOURCE(GENERATE_STRING)
-};
-
-#define METHOD_STRING_LENGTH 3
-#define FOREACH_METHOD(MTHD) \
-			MTHD(INVALID_METHOD)  \
-			MTHD(GET)  \
-			MTHD(PUT)  \
-			MTHD(RSP)
-
-typedef enum METHOD_ENUM {
-	FOREACH_METHOD(GENERATE_ENUM)
-} method_t;
-
-static const char *METHOD_STRING[] = {
-	FOREACH_METHOD(GENERATE_STRING)
-};
 
 #define RESP_OK 200
 #define BAD_REQUEST 400  //used if the message decode fails
@@ -66,7 +44,7 @@ static const char *METHOD_STRING[] = {
 #define LOCKED 423    // used for PUT mode if the boomer_base is Active
 #define UNPROCESSABLE_ENTITY 422  //used for encode errors - there was not a great error response for this
 
-#define BIPC_HEADER_LENGTH 5 + METHOD_STRING_LENGTH + 1 + RSRC_STRING_LENGTH + 1 //"BIPC PUT START "
+//-=-=- end of exported defines
 
 #define BASE_ACTIVE ((game_state != IDLE_GS) || (drill_state != IDLE_DS))
 
